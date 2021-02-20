6bc33f073b97067ab5840e7181fd9a6053ba9a98
Cameron <cway140@gmail.com>
2021-02-20 15:03:58 -0800

    Added: db files to .gitignore

--- backend/chatbot.py
+++ backend/chatbot.py
@@ -9,7 +9,6 @@ chatbot = ChatBot(
     "Test",
     logic_adapters=[
         "chatterbot.logic.MathematicalEvaluation",
-        "chatterbot.logic.TimeLogicAdapter",
         {
         "import_path": "chatterbot.logic.BestMatch",
         "response_selection_method": get_random_response ,
