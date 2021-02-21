63fd1360ebe70ede7c5fa4483b745ac861ef0272
Cameron <cway140@gmail.com>
2021-02-20 23:36:09 -0800

    Imported chatbot from chatbot.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 from flask import Flask, render_template, request, jsonify
 from chatbot import chatbot
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 
 @app.route("/")
 def home():
